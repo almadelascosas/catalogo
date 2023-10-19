3a45f741d4e775158d8084c88284ddd8fe7b3aea
yepagu <yepagu@gmail.com>
2023-10-19 11:28:46 -0500

    Config / Database: configuracion nueva base de datos

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ date_default_timezone_set("America/Bogota");
 | a PHP script and you can easily do that on your own.
 |
 */
-  $config['base_url'] = 'http://pruebas.almadelascosas.lc';
+  $config['base_url'] = 'http://catalogo.almadelascosas.lc';
 //$config['base_url'] = 'https://almadelascosas.com';
 /*
 |--------------------------------------------------------------------------
